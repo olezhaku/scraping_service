daec11e292da782d2dec426ee92e4a075a547ac0
olezhaku <139953855+olezhaku@users.noreply.github.com>
2024-08-23 21:42:09 +0900

    autorisation

--- scraping/views.py
+++ scraping/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
 from scraping.forms import FingForm
 from .models import Vacancy
 
 
 def home_view(request):
     form = FingForm()
+    return render(request, "scraping/home.html", {"form": form})
+
+
+def list_view(request):
+    form = FingForm()
 
     city = request.GET.get("city")
     language = request.GET.get("language")
-    qs = []
+
+    context = {"city": city, "language": language, "form": form}
 
     if city or language:
         _filter = {}
@@ -22,4 +28,9 @@ def home_view(request):
 
         qs = Vacancy.objects.filter(**_filter)
 
-    return render(request, "scraping/home.html", {"object_list": qs, "form": form})
+        paginator = Paginator(qs, 10)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+        context["object_list"] = page_obj
+
+    return render(request, "scraping/list.html", context)
